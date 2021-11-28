522a9590eaed24cc58a5967b6f8d994bea9f470f
Kerosin3 <keromvp@gmail.com>
2021-11-28 20:31:40 +0300

    added hw5,6

---
+++ hw5_2/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-reload",["= 0.0.1"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.30.0"]]],"checksum":"a719d14bac446633af99fa2b26314dd9181d50de26157c1150345b7b68b48fdb","vagrant_version":"2.2.19"}
