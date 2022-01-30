4d4e844fdd20b3f4b75754dab35958d71e75b918
Kerosin3 <keromvp@gmail.com>
2022-01-30 19:33:03 +0300

    script v4

--- hw8/test.sh
+++ hw8/test.sh
@@ -1,2 +1,13 @@
 #/bin/bash
-export TEST=111
+export TEST='15/Aug/2019:00:24:38 +0300'
+export TEST2='15/Aug/2019:00:22:38 +0300'
+if [[ $TEST < $TEST2 ]] # if current iteration date is greater than NOW, then break and start from this date
+	  	then
+			echo 'OKKKKKKKKKKKK'  #
+	#		break;
+		else
+			echo 'NO OKKKKKKKKKKKKKK'
+		#else
+		#	j=$(expr $i - 1)
+		#	start_analysis=$j
+	  fi
